51b72ea84c6829e07e44f9f1903345e00e5bc1cc
Haidai Kostiantyn <thekos0596@gmail.com>
2023-03-12 23:41:50 +0200

    update

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import ImagesApiService from './js/images-service';
-// import LoadMoreBtn from './js/load-more';
+
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
